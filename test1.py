744a38087d1106da89beb68873999e5184504e4f
Frepap3 <62154673+Frepap3@users.noreply.github.com>
2021-09-14 13:15:51 -0500

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+if st.button('___ it up'):
+#     st.video('/Users/corprey/Desktop/tenor (1).gif')
+    
+#     st.video('https://youtu.be/P5mtclwloEQ',start_time=100)
+    st.markdown("![Alt Text](https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif)")
